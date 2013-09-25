c5a521c6a44391e472846b4418f6b1b977edde12
mati <mati@s09-pc03>
2013-09-25 11:36:54 +0200

    Ejercico con ramas2

--- Saludogit/src/java/Saludar/saludar.java
+++ Saludogit/src/java/Saludar/saludar.java
@@ -31,7 +31,7 @@ public class saludar extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-         System.out.println("Hola mundo");
+         System.out.println("Hola mundo de nuevo");
          
         try {
             /* TODO output your page here. You may use following sample code. */
